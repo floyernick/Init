a57d7cf36eeec3d0aa0f284305e533f13d815abd
Mykyta Paliienko <floyernick@gmail.com>
2018-10-02 00:55:32 +0300

    Fix styling

--- env/transfer/api/users.go
+++ env/transfer/api/users.go
@@ -6,16 +6,23 @@ import (
 )
 
 func (api *API) UsersGet(w http.ResponseWriter, r *http.Request) {
+
 	var req request.UsersGet
+
 	err := ProcessRequest(r, &req, api.checkHash, api.hashSalt)
+
 	if err != nil {
 		ReturnErrorResponse(w, "invalid request data")
 		return
 	}
+
 	res, err := api.controller.UsersGet(req)
+
 	if err != nil {
 		ReturnErrorResponse(w, err.Error())
 		return
 	}
+
 	ReturnSuccessResponse(w, res)
+
 }
